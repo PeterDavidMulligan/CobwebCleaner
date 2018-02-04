fde24df4c81763a2122569bfdbfadb09c2fed74a
Peter Mulligan <aezur20@gmail.com>
2018-02-04 11:33:18 +0000

    Added LauncherActivity, xml layout, and manifest entry

---
+++ app/src/main/java/eu/flatworks/cobwebcleaner/LauncherActivity.java
@@ -0,0 +1,18 @@
+/*
+ * Created on 04/02/18 11:25 by Peter Mulligan.
+ * Copyright (c) 2018.
+ */
+
+package eu.flatworks.cobwebcleaner;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class LauncherActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_launcher);
+    }
+}
