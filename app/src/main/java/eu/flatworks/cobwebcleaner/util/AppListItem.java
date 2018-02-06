1f4c283bb4fbb7e1d920402766886968904c291c
Peter Mulligan <aezur20@gmail.com>
2018-02-06 09:43:18 +0000

    Added more fields and their corresponding getters/setters

--- app/src/main/java/eu/flatworks/cobwebcleaner/util/AppListItem.java
+++ app/src/main/java/eu/flatworks/cobwebcleaner/util/AppListItem.java
@@ -7,28 +7,35 @@ package eu.flatworks.cobwebcleaner.util;
 
 import android.graphics.drawable.Drawable;
 
+/**
+ * AppListItem contains all the details needed for a single
+ * user app installed on the device.
+ */
 public class AppListItem {
     private String mName;
+    private long mLastUsedTime;
+    private long mTotalUptime;
     Drawable mIcon;
 
-    public AppListItem(String name, Drawable icon) {
+    public AppListItem(String name, Drawable icon, long lastUsed, long totalUptime) {
         mName = name;
         mIcon = icon;
+        mLastUsedTime = lastUsed;
+        mTotalUptime = totalUptime;
     }
 
     public String getName() {
         return mName;
     }
-
     public void setName(String name) {
         this.mName = name;
     }
-
     public Drawable getIcon() {
         return mIcon;
     }
-
-    public void setIcon(Drawable icon) {
-        this.mIcon = icon;
-    }
+    public void setIcon(Drawable icon) { this.mIcon = icon; }
+    public long getLastUsedTime() { return mLastUsedTime; }
+    public void setLastUsedTime(long mLastUsedTime) { this.mLastUsedTime = mLastUsedTime; }
+    public long getTotalUptime() { return mTotalUptime; }
+    public void setTotalUptime(long mTotalUptime) { this.mTotalUptime = mTotalUptime; }
 }
